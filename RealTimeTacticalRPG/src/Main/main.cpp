a50fbbae0565fdf0b28f737714e019db061dc576
occuria <stevan.coroller@telecom-sudparis.eu>
2018-06-13 20:50:02 +0200

    adds three cells assets

--- RealTimeTacticalRPG/src/Main/main.cpp
+++ RealTimeTacticalRPG/src/Main/main.cpp
@@ -1,10 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include "../ResourceHandling/TextureHolder.hpp"
 
 const int width = 1600;
 const int height = 900;
 
 int main()
 {
+	/* Load textures */
+	TextureHolder holder;
+	holder.load(Textures::Block, "../../assets/cells/rocky.png");
+	holder.load(Textures::Free, "../../assets/cells/ground.png");
+	
 	sf::RenderWindow window(sf::VideoMode(width, height), "Real Time Tactical RPG", sf::Style::Fullscreen);
 
 	while (window.isOpen())
